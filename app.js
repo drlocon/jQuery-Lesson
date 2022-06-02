c5860a102ccb92a65fe43024cc62a5545074312e
karanashi <104292397+drlocon@users.noreply.github.com>
2022-06-02 18:30:03 +0900

    [Finish]5

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-//bodyタグ内のHTMLを、<h1>Hello JQuery!!</h1>で書き換えなさい
-$(document).ready(function() {
-  $('body').html('<h1>Hello jQuery!!</h1>');
-});
+//4章の内容
+
+// bodyタグ内のHTMLを、<h1>Hello JQuery!!</h1>で書き換えなさい
+// $(document).ready(function() {
+//  $('body').html('<h1>Hello jQuery!!</h1>');
+// });
 
 // 省略形
 // $(function()
@@ -10,3 +12,59 @@ $(document).ready(function() {
 
 // 基本書式
 // $(セレクタ).メソッド(パラメータ);
+
+//**************************************************************//
+
+// 5章の内容
+
+// jQueryでCSSスタイルを変更する：CSSメソッド【単数のプロパティ】
+// $('セレクタ').css('background-color', '#0000FF')
+
+// jQueryでCSSスタイルを変更する：CSSメソッド【複数のプロパティ】
+// $('セレクタ').css({
+//  'プロパティ1' :'値1',
+//  'プロパティ2' :'値2',
+//  :
+//  :
+//  'プロパティn' :'値n',
+// })
+
+//複数のプロパティの例
+// $(function(){
+//  $('.box1').css({
+//    'background-color': '#0000FF',
+//    'height': '100px'
+//  });
+// });
+
+// 上から下へアニメーション
+// $(function(){
+//   $('.box1').slideDown();
+// });
+
+// 下から上へアニメーション
+// $(function(){
+//   $('.box1').slideUp();
+// });
+
+//非表示の要素をjQueryで表示させる
+// $(function(){
+//   $('.box1').show();
+//   $('.box1').css({'background-color': '#0000FF'});
+// });
+
+//表示されている要素をjQueryで非表示にする
+// $(function(){
+//   $('.box1').hide();
+// });
+
+//確認問題
+$(function (){
+  $('.box1').slideDown(function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp();
+  });
+});
